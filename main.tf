0119d73d7995b8d6cd893bd93d8f5ba01798d923
sfc-gh-nrajaofetra <94199571+sfc-gh-nrajaofetra@users.noreply.github.com>
2022-06-06 17:15:15 +0200

    modif main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,9 @@ resource "snowflake_database" "nra_terraform_git_db" {
   name    = "NRA_TERRAFORM_GIT_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "tf_demo_schema" {
+  database = snowflake_database.nra_terraform_git_db.name
+  name     = "TF_DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
